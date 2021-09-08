3eea8df4e6a13de190b780de64631f678227bc66
Antonio Bennett <bennettantonio199@gmail.com>
2021-09-08 04:45:40 -0400

    basic file reading

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
-fn main() {
-    println!("Hello, world!");
+use std::{env, fs, io::Result, process};
+
+fn main() -> Result<()> {
+    //Do not run the program until input is specified
+    if env::args().count() == 1 {
+        println!(
+            "Please enter an existing directory or file location as an argument. Ex: rssg test.txt"
+        );
+        process::exit(1);
+    }
+
+    //Arguments passed to program
+    let args: Vec<String> = env::args().collect();
+
+    //Skip the first argument which is the executable and proceed
+    args.iter().skip(1).for_each(|arg| {
+        if let Ok(file) = fs::read_to_string(arg.to_owned()) {
+            println!("{}", file);
+        } else {
+            println!("File named {} not found", arg);
+        }
+    });
+
+    Ok(())
 }
