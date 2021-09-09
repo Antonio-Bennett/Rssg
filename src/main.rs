808c555702d35b4cb137542268d1c720c448051e
Antonio Bennett <bennettantonio199@gmail.com>
2021-09-08 23:57:11 -0400

    Basic flag options completed

--- src/main.rs
+++ src/main.rs
@@ -3,23 +3,45 @@ use std::{env, fs, io::Result, process};
 fn main() -> Result<()> {
     //Do not run the program until input is specified
     if env::args().count() == 1 {
-        println!(
-            "Please enter an existing directory or file location as an argument. Ex: rssg test.txt"
-        );
+        println!("Please enter input. Type rssg --help or -h for more information.");
         process::exit(1);
     }
 
     //Arguments passed to program
-    let args: Vec<String> = env::args().collect();
+    let args: Vec<String> = env::args().into_iter().skip(1).collect();
 
-    //Skip the first argument which is the executable and proceed
-    args.iter().skip(1).for_each(|arg| {
+    //Matches passed flag and does appropiate action
+    match &args[0] as &str {
+        "-i" | "--input" => run(&args[1..]), //Pass args starting at 1 since 0 is the flag
+        "-h" | "--help" => help_info(),
+        "-v" | "--version" => println!("rssg current version: {}", env!("CARGO_PKG_VERSION")),
+        _ => {
+            println!("Please enter a valid flag. Type rssg --help or -h for more information.");
+            process::exit(1)
+        }
+    }
+
+    Ok(())
+}
+
+fn run(args: &[String]) {
+    //Iterate through each input and process
+    args.iter().for_each(|arg| {
         if let Ok(file) = fs::read_to_string(arg.to_owned()) {
             println!("{}", file);
         } else {
             println!("File named {} not found", arg);
         }
     });
+}
 
-    Ok(())
+//Prints Help information
+fn help_info() {
+    println!("Rust Static Site Generator - RSSG");
+    println!("\nUSAGE:");
+    println!("  rssg [OPTIONS] [DIRECTORY/FILES]");
+    println!("\nOPTIONS:");
+    println!("  -h, --help         Prints help information");
+    println!("  -v, --version      Prints rssg version");
+    println!("  -i, --input        Specifies input to be used - directory or files")
 }
